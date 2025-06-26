9955d217b7b9d1835a3f9f8416dfde55cdc54944
abhignamargam <abhignamargam1975@gmail.com>
2025-06-25 21:36:39 -0500

    Add github actions deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/'
+  // Remove or comment out the base path
+  // base: '/My-Portfolio/'
 })
