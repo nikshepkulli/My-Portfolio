b6f72c59738eba6ce7f6fdbcda715c5243a0414c
abhignamargam <abhignamargam1975@gmail.com>
2025-06-25 22:02:35 -0500

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  // Remove or comment out the base path
-  // base: '/My-Portfolio/'
+  base: '/My-Portfolio/'
 })
