e2f2b496de24235c205454d914265fa493696aad
abhignamargam <abhignamargam1975@gmail.com>
2025-07-02 14:22:18 -0500

    Fix GitHub Pages deployment configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'  // or whatever your main CSS file is called
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
