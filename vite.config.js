64226a5ff11db5e3e02d0881d73ab09f68bce0a7
abhignamargam <abhignamargam1975@gmail.com>
2025-07-02 16:26:18 -0500

    fix?

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/',
+  base: '/', // Change this from '/My-Portfolio/' to '/'
   build: {
     rollupOptions: {
       output: {
