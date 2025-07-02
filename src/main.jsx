5dcd6babe86902d63dc2235b433f68bd98c51deb
abhignamargam <abhignamargam1975@gmail.com>
2025-07-02 16:05:21 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'  // or whatever your main CSS file is called
-import './styles/global.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'  // ✅ This file exists in your src folder
+import './App.css'    // ✅ This file exists in your src folder
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
