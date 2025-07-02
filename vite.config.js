44f56ff2411fbdf476fec70257f09bccb1a8c045
abhignamargam <abhignamargam1975@gmail.com>
2025-07-02 14:38:28 -0500

    quick fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/'
+  base: '/My-Portfolio/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
